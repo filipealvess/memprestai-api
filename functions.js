2532517e3ad723c9ca6ae6f53cc9f18374293759
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-01-16 15:14:29 -0300

    Parse formated dates

--- functions.js
+++ functions.js
@@ -1,10 +1,29 @@
 import reader from "xlsx";
 
-function getSpreadsheetData(sheetPageIndex = 0) {
+function getSpreadsheetData(sheetPageIndex = 0, parseColumns = []) {
   const file = reader.readFile('./spreadsheet.xlsx');
   const sheetNames = file.SheetNames;
   const sheets = file.Sheets[sheetNames[sheetPageIndex]];
-  const data = reader.utils.sheet_to_json(sheets);
+  const sheetData = reader.utils.sheet_to_json(sheets);
+
+  return parseSheetData(sheetData, parseColumns);
+}
+
+function parseSheetData(sheetData, parseColumns) {
+  const data = sheetData.map(row => {
+    for (const columnName in row) {
+      parseColumns.forEach(parseColumn => {
+        if (columnName === parseColumn) {
+          const { y, m, d } = reader.SSF.parse_date_code(row[columnName]);
+          const date = new Date(y, m, d);
+
+          row[columnName] = date;
+        }
+      });
+    }
+
+    return row;
+  });
 
   return data;
 }
--- routes.js
+++ routes.js
@@ -3,19 +3,21 @@ import getSpreadsheetData from "./functions.js";
 
 const router = express.Router();
 
-router.get('/leases', (request, response) => {
+router.get('/leases', (_, response) => {
   const leasesPageIndex = 0;
+  const parseColumns = ['Data_Locacao', 'Data_Devolucao'];
 
-  return response.json(getSpreadsheetData(leasesPageIndex));
+  return response.json(getSpreadsheetData(leasesPageIndex, parseColumns));
 });
 
-router.get('/clients', (request, response) => {
+router.get('/clients', (_, response) => {
   const clientsPageIndex = 1;
+  const parseColumns = ['Data_Nascimento'];
 
-  return response.json(getSpreadsheetData(clientsPageIndex));
+  return response.json(getSpreadsheetData(clientsPageIndex, parseColumns));
 });
 
-router.get('/movies', (request, response) => {
+router.get('/movies', (_, response) => {
   const moviesPageIndex = 2;
 
   return response.json(getSpreadsheetData(moviesPageIndex));
